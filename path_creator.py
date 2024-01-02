315317852cef4146c746e7c9196e68769201e1b3
Bohdan Marko <goldenbohdanm@gmail.com>
2024-01-02 15:28:26 +0200

    Update a image_file_path function
    Add a dir in path to separate objects in different directories

--- path_creator.py
+++ path_creator.py
@@ -6,7 +6,7 @@ from django.utils.text import slugify
 
 def image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
-    directory = slugify(instance)
+    directory = slugify(type(instance))
     filename = f"{slugify(instance)}-{uuid.uuid4()}{extension}"
 
     return os.path.join("uploads", "social_media", directory, filename)
