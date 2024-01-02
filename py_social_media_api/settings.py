f379a09f1b86de9ca120cb02172e66c98bacb289
Bohdan Marko <goldenbohdanm@gmail.com>
2024-01-02 15:08:44 +0200

    Configure AUTH_USER_MODEL & MEDIA_ROOT & MEDIA_URL in settings.py

--- py_social_media_api/settings.py
+++ py_social_media_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "user.User"
