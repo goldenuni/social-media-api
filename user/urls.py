123fd68387f76e8f9cf2c564e474e46819d34f44
Bohdan Marko <goldenbohdanm@gmail.com>
2024-01-10 23:16:37 +0200

    Add a logout feature
    Configure settings, urls, views for logout endpoint

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,22 @@
+from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
-from user.views import UserViewSet
+from user.views import UserViewSet, LogoutView
 
 app_name = "user"
 
 router = routers.DefaultRouter()
 router.register("", UserViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("api/logout/", LogoutView.as_view(), name="auth_logout"),
+    path("", include(router.ursl)),
+]
--- user/views.py
+++ user/views.py
@@ -1,8 +1,12 @@
 from django.db.models import Count
 from django.contrib.auth import get_user_model
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from user.models import Follow
 from user.permissions import IsOwnerOrReadOnly
@@ -119,3 +123,26 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    @extend_schema(
+        description="Endpoint to invalidate a refresh token and log the user out.",
+        request={"refresh_token": "string"},
+        responses={
+            205: None,
+            400: "Bad Request",
+            401: "Unauthorized",
+        },
+    )
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
